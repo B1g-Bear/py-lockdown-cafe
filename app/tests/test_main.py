a96c0b8d2ecb4214c9bdbf4cb642f8f9fd39e490
Andrii Novikov <forytube2023@gmail.com>
2025-07-01 14:33:46 +0300

    Update test_main.py

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -1,56 +1,75 @@
 import datetime
-
 import pytest
 
-from cafe import Cafe
-from main import go_to_cafe
+from app import main
+from app.cafe import Cafe
+from app.errors import NotVaccinatedError, OutdatedVaccineError, NotWearingMaskError
 
 
-def test_go_to_cafe_all_ok():
-    friends = [
-        {
-            "name": "Alisa",
-            "vaccine": {"expiration_date": datetime.date.today()},
-            "wearing_a_mask": True,
-        },
-        {
-            "name": "Bob",
-            "vaccine": {"expiration_date": datetime.date.today()},
-            "wearing_a_mask": True,
-        },
-    ]
+def test_visit_cafe_success():
     cafe = Cafe("KFC")
-    assert go_to_cafe(friends, cafe) == "Friends can go to KFC"
+    visitor = {
+        "name": "Paul",
+        "age": 23,
+        "vaccine": {"expiration_date": datetime.date.today()},
+        "wearing_a_mask": True,
+    }
+    assert cafe.visit_cafe(visitor) == "Welcome to KFC"
 
 
-def test_go_to_cafe_not_vaccinated():
-    friends = [
-        {
-            "name": "Alisa",
-            "wearing_a_mask": True,
-        },
-        {
-            "name": "Bob",
-            "vaccine": {"expiration_date": datetime.date.today()},
-            "wearing_a_mask": True,
-        },
-    ]
+def test_visit_cafe_no_vaccine():
     cafe = Cafe("KFC")
-    assert go_to_cafe(friends, cafe) == "All friends should be vaccinated"
+    visitor = {"name": "Paul", "age": 23}
+    with pytest.raises(NotVaccinatedError):
+        cafe.visit_cafe(visitor)
 
 
-def test_go_to_cafe_need_masks():
-    friends = [
-        {
-            "name": "Alisa",
-            "vaccine": {"expiration_date": datetime.date.today()},
-            "wearing_a_mask": False,
-        },
-        {
-            "name": "Bob",
-            "vaccine": {"expiration_date": datetime.date.today()},
-            "wearing_a_mask": False,
-        },
-    ]
+def test_visit_cafe_outdated_vaccine():
     cafe = Cafe("KFC")
-    assert go_to_cafe(friends, cafe) == "Friends should buy 2 masks"
+    visitor = {
+        "name": "Paul",
+        "age": 23,
+        "vaccine": {"expiration_date": datetime.date(2019, 2, 23)},
+        "wearing_a_mask": True,
+    }
+    with pytest.raises(OutdatedVaccineError):
+        cafe.visit_cafe(visitor)
+
+
+def test_visit_cafe_no_mask():
+    cafe = Cafe("KFC")
+    visitor = {
+        "name": "Paul",
+        "age": 23,
+        "vaccine": {"expiration_date": datetime.date.today()},
+        "wearing_a_mask": False,
+    }
+    with pytest.raises(NotWearingMaskError):
+        cafe.visit_cafe(visitor)
+
+
+def test_go_to_cafe_all_good():
+    cafe = Cafe("KFC")
+    friends = [
+        {"name": "Alisa", "vaccine": {"expiration_date": datetime.date.today()}, "wearing_a_mask": True},
+        {"name": "Bob", "vaccine": {"expiration_date": datetime.date.today()}, "wearing_a_mask": True},
+    ]
+    assert main.go_to_cafe(friends, cafe) == "Friends can go to KFC"
+
+
+def test_go_to_cafe_some_no_mask():
+    cafe = Cafe("KFC")
+    friends = [
+        {"name": "Alisa", "vaccine": {"expiration_date": datetime.date.today()}, "wearing_a_mask": False},
+        {"name": "Bob", "vaccine": {"expiration_date": datetime.date.today()}, "wearing_a_mask": False},
+    ]
+    assert main.go_to_cafe(friends, cafe) == "Friends should buy 2 masks"
+
+
+def test_go_to_cafe_some_no_vaccine():
+    cafe = Cafe("KFC")
+    friends = [
+        {"name": "Alisa", "wearing_a_mask": True},
+        {"name": "Bob", "vaccine": {"expiration_date": datetime.date.today()}, "wearing_a_mask": True},
+    ]
+    assert main.go_to_cafe(friends, cafe) == "All friends should be vaccinated"
