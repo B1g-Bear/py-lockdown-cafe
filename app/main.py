c66a77e62fc78e2ef8dd221750714a12420c3cc3
Andrii Novikov <forytube2023@gmail.com>
2025-07-01 14:33:44 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,26 +1,16 @@
-from cafe import Cafe
-from errors import VaccineError
+from app.cafe import Cafe
+from app.errors import VaccineError, NotWearingMaskError
 
 
 def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
     masks_to_buy = 0
 
-    try:
-        for friend in friends:
-            cafe.visit_cafe(friend)
-    except VaccineError:
-        return "All friends should be vaccinated"
-
     for friend in friends:
-        if "vaccine" not in friend:
+        try:
+            cafe.visit_cafe(friend)
+        except VaccineError:
             return "All friends should be vaccinated"
-
-        expiration_date = friend["vaccine"].get("expiration_date")
-        today = __import__("datetime").date.today()
-        if expiration_date is None or expiration_date < today:
-            return "All friends should be vaccinated"
-
-        if not friend.get("wearing_a_mask", False):
+        except NotWearingMaskError:
             masks_to_buy += 1
 
     if masks_to_buy:
